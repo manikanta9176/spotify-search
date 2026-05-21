28d4530f1bc0b244174e71acdd2f83bd0b68ac5c
Manikanta Potnuru <110469998+manikanta9176@users.noreply.github.com>
2026-05-21 14:29:01 +0530

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -47,7 +47,7 @@ const Search = () => {
           placeholder="Search for music, artists, podcasts, and more..."
         />
         <button type="submit" className="search-button">
-          I Changed it
+          Just kidding
         </button>
       </form>
       {error && <div className="error-message">{error}</div>}
